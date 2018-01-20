4d6e635e6dfcbe373656466b43dd3a049b2df789
isthisnagee <isthisnagee@gmail.com>
2018-01-20 03:14:13 -0500

    initial weather api

---
+++ hive-uoft/hive-weather/functions/__main__.js
@@ -0,0 +1,29 @@
+const weather = require('weather-js');
+const { promisify } = require('util');
+
+const findWeather = promisify(weather.find);
+/**
+ * A basic Hello World function
+ * @param {string} search The location you want the weather for
+ * @returns {object}
+ */
+module.exports = async (search = 'Toronto, ON', context) => {
+  let weather = await findWeather({ search });
+  weather = weather[0];
+  return {
+    temperature: globalTemperature(+weather.current.temperature),
+    feelsLike: globalTemperature(+weather.current.feelslike),
+  };
+};
+
+function globalTemperature(fahrenheight) {
+  // cast it to a number, just in case :)
+  const f = Math.floor(+fahrenheight);
+  const c = Math.floor((f - 32) * 5 / 9);
+  const k = Math.floor(c + 273.15);
+  return {
+    f,
+    c,
+    k,
+  };
+}
