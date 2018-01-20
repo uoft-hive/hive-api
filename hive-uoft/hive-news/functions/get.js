1894a1a5c63398c642cb98fcf89230e9a7e2d6a9
isthisnagee <isthisnagee@gmail.com>
2018-01-20 17:45:52 -0500

    + news

--- hive-uoft/hive-news/functions/get.js
+++ hive-uoft/hive-news/functions/get.js
@@ -1,6 +1,7 @@
-const { promisify } = require('util');
 const OAuth = require('oauth');
-var oauth = new OAuth.OAuth(
+const { inspect } = require('util');
+
+const oauth = new OAuth.OAuth(
   'https://api.twitter.com/oauth/request_token',
   'https://api.twitter.com/oauth/access_token',
   '6MOgb0rIlVPMiyGXEkAZFyIAL',
@@ -10,37 +11,6 @@ var oauth = new OAuth.OAuth(
   'HMAC-SHA1'
 );
 
-const userToken = '2912014127-tkV6n402Tm6wzEE5k0j8llsrnAVl2s1JSQ0q96K'; //test user token
-const userSecret = 'A5Lxi1lZwe1HETS9SRKDBuFJI7EoDtI0ONTbx9qIxiwQK'; //test user secret
-
-function getPlace({ lat, long }, callback) {
-  oauth.get(
-    `https://api.twitter.com/1.1/geo/search.json?lat=${lat}&long=${long}`,
-    userToken,
-    userSecret,
-    (e, data, res) => {
-      data = JSON.parse(data);
-      if (e) {
-        console.log(e);
-        callback({
-          place: -1,
-        });
-      } else {
-        callback({ id: data.result.places[0].id });
-      }
-    }
-  );
-}
-function getNews(place, callback) {
-  console.log('place', place);
-  oauth.get(
-    `https://api.twitter.com/1.1/geo/id/${place.id}`,
-    userToken,
-    userSecret,
-    (e, data, res) => callback(e, data, res)
-  );
-}
-
 /**
  *
  * @param {string} lat The latitude
@@ -48,10 +18,30 @@ function getNews(place, callback) {
  * @returns {any}
  */
 module.exports = (lat, long, context, callback) => {
-  getPlace({ lat, long }, place => {
-    getNews(place, (e, data, res) => {
-      if (e) console.log(e);
-      callback(null, data);
-    });
+  // make sure lat and long are ints using `+lat`, `+long`
+  getNews({ lat, long }, (e, data, res) => {
+    data = JSON.parse(data);
+    callback(
+      null,
+      data.statuses.map(tweet => ({
+        location: {
+          lat, long
+        },
+        id: tweet.id,
+        link: `https://twitter.com/statuses/${tweet.id}`,
+        text: tweet.text,
+      }))
+    );
   });
 };
+
+async function getNews({ lat, long }, callback) {
+  const userToken = '2912014127-tkV6n402Tm6wzEE5k0j8llsrnAVl2s1JSQ0q96K'; //test user token
+  const userSecret = 'A5Lxi1lZwe1HETS9SRKDBuFJI7EoDtI0ONTbx9qIxiwQK'; //test user secret
+  const data = oauth.get(
+    `https://api.twitter.com/1.1/search/tweets.json?geocode=${lat},${long},1mi`,
+    userToken,
+    userSecret,
+    callback
+  );
+}
